b196d28b47f7ff6f9b2301114b3c572362e60d45
rasmi <rasmihass4@gmail.com>
2025-03-06 10:51:09 +0300

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  // Export the functions so tests can access them
+  module.exports = {
+    introduction,
+    introductionWithLanguage,
+    introductionWithLanguageOptional
+  };
+  
